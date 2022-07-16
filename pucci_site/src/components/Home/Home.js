d87c541a84373dd635e078bce4358a9130b7dac0
QuyenHuynh <quyenh@hotmail.com>
2022-07-16 01:20:30 -0400

    added carousel

--- pucci_site/src/components/Home/Home.js
+++ pucci_site/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css";
 import CardGrid from '../CardGrid/CardGrid';
+import CarouselFade from '../Carousel/Carousel';
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <div className="grid-nav-container">
         <p> NEW IN | PET COLLECTION</p>
       </div>
+
+      <CarouselFade/>
 
       <div className="card-grid-container">
         <CardGrid />
