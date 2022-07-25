976a40da73ab28ebf9c09901ad0abc76cd81d70c
Michael Babcock <babcockmichael17@gmail.com>
2022-07-25 12:24:37 -0700

    added Product Slice
    added product slice to store

---
+++ pucci_site/src/slices/productSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const productSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {
+	getProducts: (state, action) => {
+
+	}
+  }
+});
+
+export const {getProducts} = productSlice.actions
+
+export default productSlice.reducer
--- pucci_site/src/store.js
+++ pucci_site/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"
 import cartReducer from './slices/cartSlice'
+import productSlice from "./slices/productSlice";
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
-		cart: cartReducer
+		cart: cartReducer,
+		product: productSlice
 	},
 })
